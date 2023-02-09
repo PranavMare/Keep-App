b1a1ca7d4d78b0a2603d47047a1d265cd82694ea
PranavMare <pranavmare99@gmail.com>
2023-02-08 22:49:43 -0500

    Added notes

--- src/components/Note.jsx
+++ src/components/Note.jsx
@@ -1,13 +1,24 @@
 import React from "react";
+import notes from "../notes";
 
 
-function Note(){
+function Note(props){
     return (
         <div className="note">
-            <h1>This is title</h1>
-            <p>This is content</p>
+            <h1>{props.title}</h1>
+            <p>{props.content}</p>
         </div>
     )
 };
+
+
+// notes.map(function Note(note){
+//     return (
+//         <div className="note">
+//             <h1>{note.title}</h1>
+//             <p>{note.content}</p>
+//         </div>
+//     )
+// });
 
 export default Note;
